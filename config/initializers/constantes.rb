b35055719aecb96d9a0cd090254683e51b9f3a28
thkernel <a.dembele@upcase.net>
2022-07-11 13:07:27 +0000

    first commit

---
+++ config/initializers/constantes.rb
@@ -0,0 +1,16 @@
+APPLICATION_NAME = "iKataxi"
+APPLICATION_SLOGAN = "Commandez votre taxi en toute sécurité!"
+APPLICATION_DESCRIPTION = "Plateforme de reservation de taxi à Bamako."
+APPLICATION_KEYWORDS = [""]
+
+GENDERS = [["Homme"], ["Femme"]]
+REGISTER_STATUSES = [["Ouvert"], ["Fermé"]]
+REGISTER_TYPES = [["Registre arrivée"], ["Registre départ"]]
+TASK_STATUSES = [["En attente"], ["En cours"], ["Suspendu"], ["Terminé"], ["Annulé"]]
+PRIORITIES = [["Aucun"],["Normal"], ["Basse"], ["Moyenne"], ["Haute"], ["Urgent"]]
+TICKET_STATUSES = [["Nouveau"], ["En cours"], ["Résolu"], ["Fermé"], ["Rejeté"], ["En attente"]]
+PERMISSION_ACTIONS = [["read"], ["create"], ["update"], ["destroy"]]
+DELIVERY_METHOD = [["SMTP"]]
+AUTHENTICATION_METHOD = [["Aucune"], ["Plain"], ["TLS"], ["SSL"]]
+STORAGE_SERVICES = [["Local"], ["Amazon"], ["Google Cloud"], ["Microsoft Azure"]]
+DAYS = [["Lundi"], ["Mardi"], ["Mercredi"], ["Jeudi"], ["Vendredi"], ["Samedi"], ["Dimanche"]]
